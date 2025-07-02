738b82e7732ed2a5f77ec115df3e5138390c4c25
vikky-coder-hub <vikkykumar700422@gmail.com>
2025-07-02 11:32:25 +0530

    del

--- Backend/public/api/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-const dashboardController_1 = require("../controllers/dashboardController");
-const router = (0, express_1.Router)();
-router.get('/', authMiddleware_1.authMiddleware, (0, authMiddleware_1.roleMiddleware)(['ADMIN', 'USER']), dashboardController_1.getDashboardDataHandler);
-exports.default = router;
