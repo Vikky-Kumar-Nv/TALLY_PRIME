18905c5d57e0d14b23ea83ddb953cf1edae323cb
vikky-coder-hub <vikkykumar700422@gmail.com>
2025-07-04 10:18:02 +0530

    pull complete

--- Backend/src/api/middlewares/errorHandler.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { logger } from '../utils/logger';
-
-export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  logger.error(err.message, { stack: err.stack });
-  res.status(err.status || 500).json({
-    message: err.message || 'Internal Server Error',
-  });
-};
