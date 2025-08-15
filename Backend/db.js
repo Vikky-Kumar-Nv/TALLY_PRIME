4cda3caedf413e91c3ac9ef489de06a6482c885e
vikky-coder-hub <vikkykumar700422@gmail.com>
2025-08-15 13:47:27 +0530

    add vercel.json and fix2

--- Backend/db.js
+++ Backend/db.js
@@ -5,9 +5,11 @@ const db = mysql.createPool({
   host: '192.145.238.16',
   user: 'amtbug5_usrtally',
   password: 'Tally@786$',
-  database: 'amtbug5_dbtally'
-
-  
+  database: 'amtbug5_dbtally',
+  connectionLimit: 10,
+  queueLimit: 0,
+  timeout: 60000,
+  acquireTimeout: 60000
 });
 
 module.exports = db;
